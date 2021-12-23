b73b5dfd46d7e3d58feb9b4dc730d556e2897210
Tergel Munkhdelger <tergelm@gmail.com>
2021-12-23 15:25:14 +0800

    desktop nav links

--- src/ui/Layout/Layout.tsx
+++ src/ui/Layout/Layout.tsx
@@ -3,13 +3,34 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import { useResizeDetector } from "react-resize-detector";
 
 interface LayoutProps {}
 
+const links: {
+  link: string;
+  title: string;
+}[] = [
+  {
+    link: "/about",
+    title: "About",
+  },
+  {
+    link: "/project",
+    title: "Projects",
+  },
+  {
+    link: "/contact",
+    title: "Contacts",
+  },
+];
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const { width, ref } = useResizeDetector();
+
   return (
-    <>
+    <div className="w-full h-full" ref={ref}>
       <header className="top-bar">
         <Container id="nav-container" ref={containerRef}>
           <div className="flex items-center justify-between relative py-5 z-40">
@@ -26,13 +47,27 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                 </span>
               </a>
             </Link>
-            <HamburgerMenu containerRef={containerRef} />
+            {width && width < 768 ? (
+              <HamburgerMenu containerRef={containerRef} />
+            ) : (
+              <nav className="h-12 flex items-center">
+                <ul className="nav-links">
+                  {links.map((i) => (
+                    <li key={i.link}>
+                      <Link href={i.link} passHref prefetch={false}>
+                        <a>{i.title}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )}
           </div>
         </Container>
       </header>
       <main className="pt-24">{children}</main>
       <footer></footer>
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,16 @@ module.exports = {
       transitionTimingFunction: {
         "nav-backdrop": "cubic-bezier(.83,0,.17,1)",
       },
+      animation: {
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+      },
+      keyframes: {
+        slideDownAndFade: {
+          "0%": { opacity: 0, transform: "translateY(-2px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
     },
   },
   plugins: [],
